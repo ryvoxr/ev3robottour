0c89899741f691602dc32828a2ecfa06a836adbe
hoehlrich <hoehlrich@raleighcharterhs.org>
2024-01-20 16:38:16 -0500

    yooooo it fucking works

--- main.py
+++ main.py
@@ -7,19 +7,33 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+SQUARE_SIZE = 21
+
+# action types
+STRAIGHT = 1
+TURN = 2
+
 ev3 = EV3Brick()
 gyro = GyroSensor(Port.S3)
 motor_b = Motor(Port.B)
 motor_c = Motor(Port.C)
 
-# pid_turn: face angle deg; pos is counter-clockwise
+# pid_turn: change orientation by deg; pos is counter-clockwise
 def pid_turn(deg: int):
-    kP = 3
-    kI = 0
+    kP = 5
+    kI = 0.000
     kD = 0.1
     integral = 0
     lastError = 0
-    while deg - gyro.angle() != 0:
+
+    # gyro is off by 1 degree every 90
+    if deg < 0:
+        deg += 1
+    else:
+        deg -= 1
+
+    gyro.reset_angle(0)
+    while gyro.angle() - deg != 0:
         error = deg - gyro.angle()
         integral += error
         derivative = error - lastError
@@ -51,17 +65,56 @@ def pid_straight(dist: float, speed: int):
     motor_b.brake()
     motor_c.brake()
 
-def main():
+class Action():
+    def __init__(self, action_type: int, value):
+        self.action_type = action_type
+        self.value = value
 
+    def evaluate(self):
+        if self.action_type == STRAIGHT:
+            pid_straight(self.value, 600)
+        elif self.action_type == TURN:
+            pid_turn(int(self.value))
+
+def evaluate_actions(actions: list):
+    for action in actions:
+        action.evaluate()
+        wait(100)
+
+def main():
     ev3.speaker.beep()
 
     gyro.reset_angle(0)
     motor_b.brake()
     motor_c.brake()
-    pid_straight(80, 200)
-    # wait(100)
-    # pid_turn(-90)
-    
+    actions = [
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, -90),
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, -90),
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, 90),
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, -90),
+            Action(STRAIGHT, SQUARE_SIZE*2),
+            Action(TURN, -90),
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, 180),
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, 90),
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, -90),
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, 180),
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, -90),
+            Action(STRAIGHT, SQUARE_SIZE*2),
+            Action(TURN, -90),
+            Action(STRAIGHT, SQUARE_SIZE),
+            Action(TURN, -90),
+            Action(STRAIGHT, SQUARE_SIZE),
+            ]
+    evaluate_actions(actions)
 
 if __name__ == "__main__":
     main()
