333d9087ff9bb0648d9ca5f83046072639a459d7
hoehlrich <hoehlrich@raleighcharterhs.org>
2024-01-26 21:10:25 -0500

    night before CARSO

--- main.py
+++ main.py
@@ -7,20 +7,27 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-SQUARE_SIZE = 20.25
+# target time in seconds
+TARGET_TIME = 54
 
-# action types
-STRAIGHT = 1
-TURN = 2
+
+SS = 20.25  # square size
+DRV = 1     # drive action
+TRN = 2     # turn action
+R = -90     # right turn
+L = 90      # left turn
+
+IN_PER_ROT = 10.21
 
 ev3 = EV3Brick()
 gyro = GyroSensor(Port.S3)
 motor_b = Motor(Port.B)
 motor_c = Motor(Port.C)
+timer = StopWatch()
 
 # pid_turn: change orientation by deg; pos is counter-clockwise
 def pid_turn(deg: int):
-    kP = 5
+    kP = 4
     kI = 0.000
     kD = 0.2
     integral = 0
@@ -40,20 +47,18 @@ def pid_turn(deg: int):
     motor_c.brake()
 
 def pid_straight(dist: float, speed: int):
-    kP = 3
+    kP = 2.5
     kI = 0.001
     kD = 0
     integral = 0
     lastError = 0
 
-    heading = gyro.angle()
-    
-    in_per_rot = 10.21
-    deg = (dist / in_per_rot) * 360
+    gyro.reset_angle(0)
+    deg = (dist / IN_PER_ROT) * 360
     motor_b.reset_angle(0)
     motor_c.reset_angle(0)
-    while (motor_b.angle() + motor_c.angle()) / 2 < deg:
-        error = gyro.angle() - heading
+    while abs(motor_b.angle() + motor_c.angle()) / 2 < abs(deg):
+        error = gyro.angle()
         integral += error
         derivative = error - lastError
         turn = kP * error + kI * integral + kD * derivative
@@ -69,15 +74,31 @@ class Action():
         self.value = value
 
     def evaluate(self):
-        if self.action_type == STRAIGHT:
+        if self.action_type == DRV:
             pid_straight(self.value, 600)
-        elif self.action_type == TURN:
+        elif self.action_type == TRN:
             pid_turn(int(self.value))
 
 def evaluate_actions(actions: list):
     for action in actions:
         action.evaluate()
         wait(100)
+
+def final_drive():
+    time_remaining = TARGET_TIME - timer.time()/1000
+    if time_remaining < 0:
+        pid_straight(SS*.85, 600)
+        return
+
+    ang_vel = (SS*.85 * 360) / (IN_PER_ROT * time_remaining)
+    pid_straight(SS*.85, int(ang_vel))
+    while timer.time()/1000 < TARGET_TIME:
+        motor_b.run(5)
+        motor_c.run(5)
+
+def first_drive():
+    pid_straight(SS*0.25, -300)
+    wait(100)
 
 def main():
     ev3.speaker.beep()
@@ -86,27 +107,44 @@ def main():
     motor_b.brake()
     motor_c.brake()
     actions = [
-            Action(STRAIGHT, SQUARE_SIZE),
-            Action(TURN, 90),
-            Action(STRAIGHT, SQUARE_SIZE),
-            Action(TURN, -90),
-            Action(STRAIGHT, SQUARE_SIZE*2),
-            Action(TURN, 180),
-            Action(STRAIGHT, SQUARE_SIZE*2),
-            Action(TURN, 90),
-            Action(STRAIGHT, SQUARE_SIZE*3),
-            Action(TURN, 90),
-            Action(STRAIGHT, SQUARE_SIZE*2),
-            Action(TURN, 180),
-            Action(STRAIGHT, SQUARE_SIZE),
-            Action(TURN, -90),
-            Action(STRAIGHT, SQUARE_SIZE),
-            Action(TURN, -90),
-            Action(STRAIGHT, SQUARE_SIZE),
-            Action(TURN, 90),
-            Action(STRAIGHT, SQUARE_SIZE*.75),
+            Action(TRN, R),
+            Action(DRV, SS*2),
+            Action(TRN, 180),
+            Action(DRV, SS*2),
+            Action(TRN, L),
+            Action(DRV, SS*3),
+            Action(TRN, R),
+            Action(DRV, SS),
+            Action(TRN, R),
+            Action(DRV, SS*2),
+            Action(TRN, 180),
+            Action(DRV, SS*2),
+            Action(TRN, L),
+            Action(DRV, SS),
+            Action(TRN, L),
+            Action(DRV, SS),
+            Action(TRN, R),
+            Action(DRV, SS),
+            Action(TRN, R),
+            Action(DRV, SS),
+            Action(TRN, L),
+            Action(DRV, SS),
+            Action(TRN, L),
+            Action(DRV, SS),
+            Action(TRN, 180),
+            Action(DRV, SS),
+            Action(TRN, R),
+            Action(DRV, SS),
+            Action(TRN, R),
+            Action(DRV, SS),
+            Action(TRN, L),
+            Action(DRV, SS),
+            Action(TRN, R),
             ]
+    timer.reset()
+    first_drive()
     evaluate_actions(actions)
+    final_drive()
 
 if __name__ == "__main__":
     main()
